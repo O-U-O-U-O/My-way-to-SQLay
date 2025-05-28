ee6f28559f823b98f1c0775f02e26f2aa2ee377e
egor evm 24216 <cq971037@gmail.com>
2025-05-28 19:04:36 +0700

    Update Chapter1.sql

--- Chapter1.sql
+++ Chapter1.sql
@@ -17,24 +17,29 @@ SELECT DISTINCT COMPANY FROM MANUFACTURER
 WHERE COMPANY LIKE 'A%' OR COMPANY LIKE 'B%'
 ORDER BY COMPANY  
 
---1.5
+--1.5 Get all the unique final products (i.e. the wares that are not in use as a material anywhere)
+  
 SELECT DISTINCT WARE
 FROM PRODUCT
 WHERE WARE NOT IN (SELECT WARE FROM MATERIAL)
 
---1.6
+--1.6 Get all the unique wares that could not be produced
+  
 SELECT DISTINCT WARE FROM CATEGORY
-WHERE WARE NOT IN (SELECT WARE FROM PRODUCT)     --1.5 & 1.6: can use EXPECT too
+WHERE WARE NOT IN (SELECT WARE FROM PRODUCT)    
 
---1.7
+--1.7 Get all the unique wares that both materials and products
+  
 SELECT DISTINCT WARE FROM MATERIAL
-WHERE WARE IN (SELECT WARE FROM PRODUCT)    --also INTERSECT
+WHERE WARE IN (SELECT WARE FROM PRODUCT)    
 
---1.8
-SELECT MIN(PRICE), MAX(PRICE)    --also UNION
+--1.8. Get the minimal and maximal prices of Paper
+  
+SELECT MIN(PRICE), MAX(PRICE)    
 FROM PRODUCT 
 
---1.9
+--1.9 Get the average price and variance price of Meat, both rounded to one decimal point.
+  
 SELECT ROUND(AVG(PRICE))
 FROM PRODUCT
 WHERE WARE = 'Meat'
